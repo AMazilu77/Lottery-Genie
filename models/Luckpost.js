1a95996ed88fbbd59a188dfd5ca4f06bbd0a7cb6
Alexander <Amazilu77@gmail.com>
2020-10-17 01:32:47 -0400

    cleared out data base and added creator key to data schema on post model

--- models/Luckpost.js
+++ models/Luckpost.js
@@ -5,7 +5,8 @@ const luckyPostSchema = mongoose.Schema({
   // ** Changed Number Selected To String **
   numberSelected: {type: String, required: true },
   reasoning: {type: String, required: false },
-  imagePath: {type: String, required: false }
+  imagePath: {type: String, required: false },
+  creator: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 // model object is what is created from the blue print, using mon
