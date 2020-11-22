76ef9b93855ef2c682238523aabca680b09012d7
Alexander <Amazilu77@gmail.com>
2020-11-22 01:40:33 -0500

    pick 2 winning numbers ts

--- src/app/services/winningNumbers.service.ts
+++ src/app/services/winningNumbers.service.ts
@@ -1,1 +1,21 @@
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import { WinningPick2NumberModel } from '../winningNumber/winningNumber.model';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+const BACKEND_URL = environment.apiURL + '/winningNumbers/FLpick2winners'
+
+//Decorator that marks a class as available to be provided and injected as a dependency.
+@Injectable({ providedIn: 'root' })
+export class winningNumbersServiceClass {
+private winningFLPick2List: WinningPick2NumberModel[] = [];
+constructor(private http: HttpClient, private router: Router) {}
+
+    getPick2Winners() {
+    return this.http.get<{ drawDate: string; winingNumber: string }>(
+        BACKEND_URL
+    )}
+
+}
--- src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
+++ src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { winningNumbersServiceClass } from '../../../../../services/winningNumbers.service';
+import { WinningPick2NumberModel } from '../../../../winningNumber.model';
 
 @Component({
   selector: 'app-pick2-winning-numbers',
@@ -7,8 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./pick2-winning-numbers.component.scss']
 })
 export class Pick2WinningNumbersComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  isLoading = false;
+  winningPick2Numbers:WinningPick2NumberModel[] = [] 
+  constructor(private router: Router, WinningService: winningNumbersServiceClass) { }
 
   ngOnInit() {
   }
