d5daceacbe71d44aed6f0982c9c8237d11f5113c
Alexander <Amazilu77@gmail.com>
2020-11-22 18:23:01 -0500

    pick 2 evening numbers hard added- not from DB logic

--- src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
+++ src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
@@ -25,6 +25,20 @@ export class Pick2WinningNumbersComponent implements OnInit {
     {drawDate: 'Nov 13, 2020', winningNumber:'97', midDay: true, evening: false},
     {drawDate: 'Nov 12, 2020', winningNumber:'52', midDay: true, evening: false},
     {drawDate: 'Nov 11, 2020', winningNumber:'02', midDay: true, evening: false},
+    {drawDate: 'Nov 10, 2020', winningNumber:'17', midDay: true, evening: false},
+    {drawDate: 'Nov 09, 2020', winningNumber:'64', midDay: true, evening: false},
+
+  ];
+   pick2EveningWinnersDemoData = [
+
+    {drawDate: 'Nov 16, 2020', winningNumber:'73', midDay: false, evening: true},
+    {drawDate: 'Nov 15, 2020', winningNumber:'31', midDay: false, evening: true},
+    {drawDate: 'Nov 14, 2020', winningNumber:'35', midDay: false, evening: true},
+    {drawDate: 'Nov 13, 2020', winningNumber:'72', midDay: false, evening: true},
+    {drawDate: 'Nov 12, 2020', winningNumber:'36', midDay: false, evening: true},
+    {drawDate: 'Nov 11, 2020', winningNumber:'33', midDay: false, evening: true},
+    {drawDate: 'Nov 10, 2020', winningNumber:'09', midDay: false, evening: true},
+    {drawDate: 'Nov 09, 2020', winningNumber:'44', midDay: false, evening: true},
 
   ];
 
