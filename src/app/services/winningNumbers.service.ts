159d23ffea7f6e1ff1973b4540758531c6c496b8
Alexander <Amazilu77@gmail.com>
2020-11-22 04:07:19 -0500

    winning pick 2 numbers loading from hardcoded demo data, not from data base and services

--- src/app/services/winningNumbers.service.ts
+++ src/app/services/winningNumbers.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 
-const BACKEND_URL = environment.apiURL + '/winningNumbers/FLpick2winners'
+const BACKEND_URL = environment.apiURL + '/winning/FLpick2winners'
 
 //Decorator that marks a class as available to be provided and injected as a dependency.
 @Injectable({ providedIn: 'root' })
@@ -14,7 +14,7 @@ private winningFLPick2List: WinningPick2NumberModel[] = [];
 constructor(private http: HttpClient, private router: Router) {}
 
     getPick2Winners() {
-    return this.http.get<{ drawDate: string; winingNumber: string }>(
+    return this.http.get<{ drawDate: string; winingNumber: string, midDay: boolean, evening: boolean }>(
         BACKEND_URL
     )}
 
--- src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
+++ src/app/winningNumber/winning-by-state/Florida/florida-winning-numbers-game-pick/pick2-winning-numbers/pick2-winning-numbers.component.ts
@@ -11,9 +11,25 @@ import { WinningPick2NumberModel } from '../../../../winningNumber.model';
 export class Pick2WinningNumbersComponent implements OnInit {
   isLoading = false;
   winningPick2Numbers:WinningPick2NumberModel[] = [] 
-  constructor(private router: Router, WinningService: winningNumbersServiceClass) { }
+  // totalPosts = 0;
+  // postsPerPage = 2;
+  // currentPage = 1;
+  // pageSizeOptions = [1, 2, 5, 10];
+  constructor(private router: Router, public WinningService: winningNumbersServiceClass) { }
+
+   pick2MidDayWinnersDemoData = [
+
+    {drawDate: 'Nov 16, 2020', winningNumber:'08', midDay: true, evening: false},
+    {drawDate: 'Nov 15, 2020', winningNumber:'65', midDay: true, evening: false},
+    {drawDate: 'Nov 14, 2020', winningNumber:'83', midDay: true, evening: false},
+    {drawDate: 'Nov 13, 2020', winningNumber:'97', midDay: true, evening: false},
+    {drawDate: 'Nov 12, 2020', winningNumber:'52', midDay: true, evening: false},
+    {drawDate: 'Nov 11, 2020', winningNumber:'02', midDay: true, evening: false},
+
+  ];
 
   ngOnInit() {
+    // this.WinningService.getPick2Winners()
   }
 
 
