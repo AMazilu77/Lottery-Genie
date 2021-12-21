5007d8121deac167807928e97841cb2dcecd0f20
Alex Mazilu <37428918+AMazilu77@users.noreply.github.com>
2021-12-21 18:40:59 -0500

    comment updates

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
+
+// leave comments here to explain all of this
 const normalizePort = val => {
   var port = parseInt(val, 10);
 
