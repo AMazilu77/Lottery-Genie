447896b4b1901ca25b026e6c95ec1009dd468026
Alex Mazilu <37428918+AMazilu77@users.noreply.github.com>
2022-05-16 20:10:12 -0400

    undoing last change

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 // static middleweare
 // app.use('/images', express.static(path.join('backend/images')));
 
-app.use(express.static(__dirname + '/dist/the-lottery-djinni/index.html'));
+app.use(express.static(__dirname + '/dist/the-lottery-djinni'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/the-lottery-djinni/index.html'));});
