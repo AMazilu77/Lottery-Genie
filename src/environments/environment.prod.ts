bb54503a04773d74277386de2bbb12f02efe41b9
Alex Mazilu <37428918+AMazilu77@users.noreply.github.com>
2025-05-12 20:51:05 -0400

    Rebuild frontend with updated API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'lottery-genie-backend-production.up.railway.app/api'
+  apiUrl: 'https://lottery-genie-backend-production.up.railway.app/api'
 };
