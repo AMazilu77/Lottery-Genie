054a2e1d7bee710d01b8b9a21e1079d1f52f27a9
Alex Mazilu <37428918+AMazilu77@users.noreply.github.com>
2025-05-28 04:07:00 -0400

    updated to test files for FL lotto

--- src/app/winningNumber/winning-by-state/Florida/fl-winning-numbers-game-pick/fl-lotto-winning-numbers/fl-lotto-winning-numbers.component.spec.ts
+++ src/app/winningNumber/winning-by-state/Florida/fl-winning-numbers-game-pick/fl-lotto-winning-numbers/fl-lotto-winning-numbers.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FLLottoWinningNumbersComponent } from './fl-lotto-winning-numbers.component';
+import { FlLottoWinningNumbersComponent } from './fl-lotto-winning-numbers.component';
 
 describe('FLLottoWinningNumbersComponent', () => {
-  let component: FLLottoWinningNumbersComponent;
-  let fixture: ComponentFixture<FLLottoWinningNumbersComponent>;
+  let component: FlLottoWinningNumbersComponent;
+  let fixture: ComponentFixture<FlLottoWinningNumbersComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FLLottoWinningNumbersComponent ]
+      declarations: [ FlLottoWinningNumbersComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FLLottoWinningNumbersComponent);
+    fixture = TestBed.createComponent(FlLottoWinningNumbersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
